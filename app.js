5a6d210d50759149986278944131d0fc642319c6
Gustavo Giacomeli <gustavo.giacomeli@hotmail.com>
2021-09-21 23:09:04 -0300

    changed appjs to just show what to do

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const processfile = require("./src/services/fileProcessing");
+/**
+ * This is just a simple entry point to tell what to do if its someone first time trying to use it
+ */
 
-processfile.readLogs("logs.txt");
+console.log(
+  "If you want to seed a database with a logfile please run npm run ProcessFile on your terminal\n"
+);
+console.log(
+  "If you already seeded please run npm run GenerateReports on your terminal"
+);
