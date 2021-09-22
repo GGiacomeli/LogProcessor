3c5b68bf3a9301591457b3207e5da870c0f244a8
Gustavo Giacomeli <gustavo.giacomeli@hotmail.com>
2021-09-21 23:23:25 -0300

    added a atribute on the pool construction so connections don't get stuck until release

--- src/repository/insertQueries.js
+++ src/repository/insertQueries.js
@@ -7,29 +7,29 @@ const { Pool } = require("pg");
  * read docs for more info
  * https://node-postgres.com/features/connecting
  */
-const pool = new Pool();
+const pool = new Pool({
+  allowExitOnIdle: true,
+});
 
 /**
  *
  * @param {authenticated_entity} value is expected to be a uuid from the log file
  */
-function insertConsumer(value) {
-  const query =
+async function insertConsumer(value) {
+  const queryString =
     "INSERT INTO consumers(authenticated_entity) VALUES ($1) ON CONFLICT DO NOTHING;";
-  pool.query(query, [value], (err) => {
-    if (err) {
-      throw new Error(err.stack);
-    }
+  await pool.query(queryString, [value], (err) => {
+    if (err) throw err.stack;
   });
 }
 /**
  *
  * @param {uuid} value is expected to be a uuid value from the authenticated_entity object from the log file
  */
-function insertService(value) {
+async function insertService(value) {
   const query =
     "INSERT INTO services(service_id) VALUES ($1) ON CONFLICT DO NOTHING;";
-  pool.query(query, [value], (err) => {
+  await pool.query(query, [value], (err) => {
     if (err) {
       throw new Error(err.stack);
     }
@@ -44,11 +44,11 @@ function insertService(value) {
  * proxy, kong and requests are expected to be int values from latencies object from the log file
  * serviceId is expected to be a uuid value from service object from the log file
  */
-function insertLatencies(proxy, kong, request, serviceId) {
+async function insertLatencies(proxy, kong, request, serviceId) {
   const values = [proxy, kong, request, serviceId];
   const query =
     "INSERT INTO latencies (proxy, kong, request, fk_service_id) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING";
-  pool.query(query, values, (err) => {
+  await pool.query(query, values, (err) => {
     if (err) {
       throw new Error(err.stack);
     }
@@ -61,11 +61,11 @@ function insertLatencies(proxy, kong, request, serviceId) {
  * service_id and authenticated_entity are expected to be uuid values from service and authenticated_entity
  * given by the log file
  */
-function insertRequest(service, consumer) {
+async function insertRequest(service, consumer) {
   const values = [service, consumer];
   const query =
     "INSERT INTO requests (fk_service_id, fk_consumer_authenticated_entity) VALUES ($1, $2) ON CONFLICT DO NOTHING";
-  pool.query(query, values, (err) => {
+  await pool.query(query, values, (err) => {
     if (err) {
       throw new Error(err.stack);
     }
