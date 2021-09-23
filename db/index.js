c5231a08da6448192ceb1b519c0416f1796ac10f
Gustavo Giacomeli <gustavo.giacomeli@hotmail.com>
2021-09-22 21:05:46 -0300

    Added some comment about the method and two atributes to the pool obj constructor

--- db/index.js
+++ db/index.js
@@ -3,13 +3,17 @@ require("dotenv").config({ path: "./src/config/.env" });
 const { Pool } = require("pg");
 
 /**
- * creates a pool Connection to the database with .env params
- * and allowExitOnIdle, this makes it that any pool to disconnect immediatly after being idle
+ * creates a pool Connection to the database with .env params,
+ * allowExitOnIdle makes it that any pool to disconnect immediatly after being idle
+ * max: defines how many clients a pool should have
  * read docs for more info
  * https://node-postgres.com/features/connecting
  * for more info about the Pool connection https://node-postgres.com/api/pool
  */
-const pool = new Pool({ allowExitOnIdle: true });
+const pool = new Pool({
+  allowExitOnIdle: true,
+  max: 25,
+});
 
 module.exports = {
   query: (text, params, callback) => pool.query(text, params, callback),
